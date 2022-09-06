5fa7c4acfd3a1f4e012393051ff2cfa00d03d01e
Amer Barnawi <104047218+amerbarnawi@users.noreply.github.com>
2022-09-07 01:20:59 +0200

    Update ex-2: create table syntax.
    Adding ( IF NOT EXISTS ) to ( create table ) syntax.

--- Week3/homework/ex-2-transactions/transactions-create-tables.js
+++ Week3/homework/ex-2-transactions/transactions-create-tables.js
@@ -1,14 +1,14 @@
 export const createTables = [
   {
     title: "Creating the account table:",
-    query: `CREATE TABLE account (
+    query: `CREATE TABLE IF NOT EXISTS account (
                 account_number INT NOT NULL PRIMARY KEY,
                 balance REAL
                 );`,
   },
   {
     title: "Creating the account_changes table:",
-    query: `CREATE TABLE account_changes (
+    query: `CREATE TABLE IF NOT EXISTS account_changes (
             change_number INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
             account_number INT NOT NULL, 
             amount REAL, 
